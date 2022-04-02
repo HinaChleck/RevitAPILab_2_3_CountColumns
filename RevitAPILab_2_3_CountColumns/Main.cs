6a38054c718b2ef3bac627d16140a7cb3de48cb3
HinaChleck <HinaChleck@gmail.com>
2022-04-02 14:23:37 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab_2_3_CountColumns/Main.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab_2_3_CountColumns
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> columns = new FilteredElementCollector(doc)
+            .OfCategory(BuiltInCategory.OST_StructuralColumns)
+            .WhereElementIsNotElementType()// не требуется при фильтрации по классу (требуется при фильтрации по категории)
+            .Cast<FamilyInstance>()
+            .ToList();
+            TaskDialog.Show("Количество колонн в проекте : ", columns.Count.ToString()); ;
+            return Result.Succeeded;
+        }
+    }
+}
